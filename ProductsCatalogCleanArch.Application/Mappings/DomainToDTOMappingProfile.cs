e7d25c2693e21abc959ff7970c34b3a4da3b59db
Wanderleyps <wanderleyps794@gmail.com>
2022-12-17 22:36:58 -0300

    Add DTOs end AutoMapping classes

---
+++ ProductsCatalogCleanArch.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProductsCatalogCleanArch.Application.DTOs;
+using ProductsCatalogCleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsCatalogCleanArch.Application.Mappings
+{
+    class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            //utiliza o ReverseMap para não precisar escrever o mapeamento reverso
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
