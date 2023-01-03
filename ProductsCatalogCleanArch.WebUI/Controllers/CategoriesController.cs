851f564806f07e77ee0e74c17a62d180e6842f32
Wanderleyps <wanderleyps794@gmail.com>
2023-01-03 19:39:03 -0300

    add delete feature in WebUI

--- ProductsCatalogCleanArch.WebUI/Controllers/CategoriesController.cs
+++ ProductsCatalogCleanArch.WebUI/Controllers/CategoriesController.cs
@@ -62,5 +62,24 @@ namespace ProductsCatalogCleanArch.WebUI.Controllers
             }
             return View(categoryDto);
         }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
     }
 }
