375e83e588fe5e972baddead40fdac4143d46d60
Wanderleyps <wanderleyps794@gmail.com>
2023-01-03 16:29:21 -0300

    remove GetProductCategory method, replace for GetById

--- ProductsCatalogCleanArch.Application/Interfaces/IProductService.cs
+++ ProductsCatalogCleanArch.Application/Interfaces/IProductService.cs
@@ -8,7 +8,6 @@ namespace ProductsCatalogCleanArch.Application.Interfaces
     {
         Task<IEnumerable<ProductDTO>> GetProducts();
         Task<ProductDTO> GetById(int? id);
-        Task<ProductDTO> GetProductCategory(int? id);
         Task Add(ProductDTO productDto);
         Task Update(ProductDTO productDto);
         Task Remove(int? id);
