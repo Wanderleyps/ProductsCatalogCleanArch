94737d2191ce7d9bd36f9a82169ca2b7ae1f253d
Wanderleyps <wanderleyps794@gmail.com>
2023-01-04 10:05:16 -0300

    regiser Identity services in IoC

--- ProductsCatalogCleanArch.Infra.IoC/DependencyInjection.cs
+++ ProductsCatalogCleanArch.Infra.IoC/DependencyInjection.cs
@@ -1,12 +1,15 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProductsCatalogCleanArch.Application.Interfaces;
 using ProductsCatalogCleanArch.Application.Mappings;
 using ProductsCatalogCleanArch.Application.Services;
+using ProductsCatalogCleanArch.Domain.Account;
 using ProductsCatalogCleanArch.Domain.Interfaces;
 using ProductsCatalogCleanArch.Infra.Data.Context;
+using ProductsCatalogCleanArch.Infra.Data.Identity;
 using ProductsCatalogCleanArch.Infra.Data.Repositories;
 using System;
 
@@ -35,13 +38,28 @@ namespace ProductsCatalogCleanArch.Infra.IoC
              options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
              //define que os arquivos de migração irão ficar na pasta onde está definido o arquivo de contexto (Infra.Data)
              b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-            
+
+            //Incluindo a configuração do Identity para users e roles
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()//inclui EF Core do Identity
+                .AddDefaultTokenProviders();//adiciona provedor de token padrão
+
+            //Configuração do cookie
+            //quando o user não tiver acesso a algum recurso, redireciona para fazer login
+            services.ConfigureApplicationCookie(options =>
+                     options.AccessDeniedPath = "/Account/Login");
+
             //AddScoped é recomendação para aplicações web
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
 
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+
             //necessário informar o nome do arquivo onde foram definidas as configurações do AutoMapper
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
