4b7b088e70306929efe5e5bbfbc08608e109158a
Wanderleyps <wanderleyps794@gmail.com>
2023-01-04 16:04:10 -0300

    add ProductController in API

--- ProductsCatalogCleanArch.Application/DTOs/ProductDTO.cs
+++ ProductsCatalogCleanArch.Application/DTOs/ProductDTO.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ProductsCatalogCleanArch.Application.DTOs
 {
@@ -38,6 +39,7 @@ namespace ProductsCatalogCleanArch.Application.DTOs
         [DisplayName("Product Image")]
         public string Image { get; set; }
 
+        [JsonIgnore]
         public Category Category { get; set; }
 
         [DisplayName("Categories")]
